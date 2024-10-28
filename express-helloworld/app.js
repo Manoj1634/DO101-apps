93ab723483fa36704554b2185a055bfec4cdb796
Manoj1634 <manojpadala1634@gmail.com>
2024-10-28 10:58:08 -0400

    newone

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,11 +1,20 @@
+var express = require('express');
+var app = express(); // Initialize the express app
 
-var response;
+var response; // Declare the response variable
 
+// Define the first route for '/'
 app.get('/', function (req, res) {
-    response = 'This is version 2 of the app.' + '\n';
-
-    //send the response to the client
-    res.send(response);
-
+    response = 'This is version 3 of the app.\n'; 
+    res.send(response); // Send response for version 2
 });
 
+// Define a second route for '/hello'
+app.get('/hello', function (req, res) {
+    res.send('Hello World!\n'); // Send response for hello route
+});
+
+// Start the server on port 8080
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
+});
